b2f4284ede0dce9ffcf35d722892f1e3cec9a2db
BussanQ <bussanq@gmail.com>
2024-11-09 22:15:24 +0800

    code

--- src/main/java/com/bussanq/kubewolf/api/controller/K8sC.java
+++ src/main/java/com/bussanq/kubewolf/api/controller/K8sC.java
@@ -1,6 +1,7 @@
 package com.bussanq.kubewolf.api.controller;
 
 import com.bussanq.kubewolf.k8s.lib.K8sService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author bussanq
  * @date 2024/11/09
  */
+@Slf4j
 @RestController
 public class K8sC {
 
@@ -17,6 +19,7 @@ public class K8sC {
 
     @GetMapping("/start")
     public String index() {
+        log.info("start");
         k8sService.createConfigMap("acs", "a", "2");
         k8sService.apply("apiVersion: v1\n" +
                 "data:\n" +
