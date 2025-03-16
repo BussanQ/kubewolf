2bf0a2f69c9c4163c1940791441a9eaa5fd4b8cd
BussanQ <bussanq@gmail.com>
2025-03-16 18:13:47 +0800

    install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-sealos
+# 系统+gpu插件
+sealos run registry.cn-beijing.aliyuncs.com/bqai/kubernetes:v1.28.9-gpu registry.cn-shanghai.aliyuncs.com/labring/calico:v3.28.1 --single
+# 算力hami + 存储seaweedfs + 网络网关aibrix
+sealos run registry.cn-beijing.aliyuncs.com/bqai/app:hami_2.4.1
+sealos run registry.cn-beijing.aliyuncs.com/bqai/app:seaweedfs_3.85 registry.cn-beijing.aliyuncs.com/bqai/app:seaweedfs-csi-driver_20250315
+sealos run registry.cn-beijing.aliyuncs.com/bqai/app:aibrix_v0.2.1
