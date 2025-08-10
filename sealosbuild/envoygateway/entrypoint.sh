3fe5e100e0ac1450962e675e6286bc10d29d0d37
BussanQ <bussanq@gmail.com>
2025-08-10 17:41:07 +0800

    update

--- sealosbuild/envoygateway/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-A="envoygateway"
-NAME=${NAME:-$A}
-NAMESPACE=${NAMESPACE:-$A}
-CHARTS=${CHARTS:-"./charts/gateway-helm-1.3.2.tgz"}
-HELM_OPTS=${HELM_OPTS:-""}
-
-helm upgrade -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
--- sealosbuild/hami/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-NAME=${NAME:-"hami"}
-NAMESPACE=${NAMESPACE:-"hami"}
-CHARTS=${CHARTS:-"./charts/hami"}
-HELM_OPTS=${HELM_OPTS:-""}
-
-helm upgrade -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
